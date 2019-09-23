98e08b0bcc29d81f95a32f30ab13d49645f02bcc
alejandro quesada tinco <tqaw19@gmail.com>
2019-09-23 17:40:27 -0500

    primer commit

---
+++ Semana05/Semana05/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Entity;
+using Business;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        public void Cargar()
+        {
+            BCategoria Bcategoria = null;
+            try
+            {
+                Bcategoria = new BCategoria();
+                dgvCategoria.ItemsSource = Bcategoria.Listar(0);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el Admin");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            ManCategoria manCategoria = new ManCategoria(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void DgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+            ManCategoria manCategoria = new ManCategoria(idCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+    }
+}
---
+++ Semana05/Semana05/ManCategoria.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Entity;
+using Business;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ManCategoria.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public int ID { get; set; }
+
+        public ManCategoria(int Id)
+        {
+            InitializeComponent();
+            ID = Id;
+            if (ID > 0)
+            {
+                BCategoria bCategoria = new BCategoria();
+                List<Categoria> categorias = new List<Categoria>();
+                categorias = bCategoria.Listar(ID);
+                if (categorias.Count > 0)
+                {
+                    lblID.Content = categorias[0].IdCategoria.ToString();
+                    txtNombre.Text = categorias[0].NombreCategoria;
+                    txtDescripcion.Text = categorias[0].Descripcion;
+                }
+            }
+        }
+
+        private void BtnGrabar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria Bcategoria = null;
+            bool result = true;
+            try
+            {
+                Bcategoria = new BCategoria();
+                if (ID > 0)
+                    result = Bcategoria.Actualizar(new Categoria { IdCategoria = ID, NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+                else
+                    result = Bcategoria.Insertar(new Categoria { NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+
+                if (!result)
+                    MessageBox.Show("Comunicarse con el admin");
+
+                Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el admin");
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+
+        private void BtnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria Bcategoria = null;
+            
+            try
+            {
+                Bcategoria = new BCategoria();
+                Bcategoria.Eliminar(ID);
+
+                    Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Comunicarse con el admin" + ex);
+            }
+            finally
+            {
+                Bcategoria = null;
+            }
+        }
+    }
+}
